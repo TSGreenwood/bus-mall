049fe242a0cc35aa768762c03836f7058264ca5e
TSGreenwood <greenwood.ts@gmail.com>
2018-12-10 18:43:59 -0800

    putting up some janky code so someone can look at it.

--- js/app.js
+++ js/app.js
@@ -1,2 +1,66 @@
 'use strict';
+var allProducts = [];
+var allProductIdNames = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'];
+console.log(allProductIdNames);
+// I'll connect this to the DOM:
+ProductVoter.imgElement = document.getElementById('qualityProduct');
+
+// I want to generate the file paths:
+// var allProductFilePaths =[];
+// for (var n = 0; n < allProductIdNames.length; n++) {
+//   allProductFilePaths[n] = allProductIdNames[n] +'.jpg';
+// }
+// console.log(allProductFilePaths[4]);
+
+function ProductVoter(idName, filepath, caption, numberClicked, numberShown) {
+  this.idName = idName;
+  this.filepath = filepath;
+  this.caption = caption;
+  // numberClicked and numberShown will need to be vars assigned to functions, or maybe prototypes
+  this.numberClicked = numberClicked;
+  this.numberShown = numberShown;
+  ProductVoter.allProducts.push(this);
+} //end constructor
+new ProductVoter('bag', '\img\bag.jpg', 'Bag', 0, 0);
+
+//randomly generate a number to use as an index value
+ProductVoter.ranNum = function() {
+  var random = Math.random() * allProducts.length;
+  var roundedDown = Math.floor(random);
+  return roundedDown;
+};
+
+ProductVoter.renderProductvoter = function() {
+  // make our randomIndex number to feed a position in array Goat.allGoats
+  var randomIndex = ProductVoter.ranNum();
+
+  // now we use that to get our random product:
+  var randomProduct = allProducts[randomIndex];
+  // randomProduct won't be an array, it'll be a new product for each event.
+  //now, modify idname:
+  ProductVoter.imgElement.idName = randomProduct.idName;
+  ProductVoter.imgElement.filepath = randomProduct.filepath;
+  ProductVoter.imgElement.caption = randomProduct.caption;
+  ProductVoter.imgElement.numberClicked = randomProduct.numberClicked;
+  ProductVoter.imgElement.numberShown = randomProduct.numberShown;
+}; //end render function
+
+//make new instances, remember that your filepath starts at the HTML document, NOT the js file. This means you don't need ..slash
+// is there a more efficient way to make my 20 new instances?
+
+
+// then I want to render 3 selected random images with buttons
+ProductVoter.renderProductvoter();
+
+// listen to the image element and add event listener, maybe I need to add three, one for each button?
+ProductVoter.imgElement.addEventListener('click', ProductVoter.randomProduct);
+//idName, filepath, caption, numberClicked, numberShown
+
+
+
+
+
+
+
+
 
--- js/constructor-example.js
+++ js/constructor-example.js
@@ -1,45 +1,95 @@
+// straight-up Allie's code:
 'use strict';
-// constructor functions to make goat instances
+
+// access the img element from the DOM
+Goat.imgElement = document.getElementById('goat-pic');
+
+// store our goats in an array
+Goat.allGoats = [];
+
+// constructor function to make Goat instances
 function Goat(filepath, description) {
   this.url = filepath;
   this.altText = description;
   Goat.allGoats.push(this);
-} //end constructor
+}
 
-//store those objects in an array
-Goat.allGoats = [];
+// make our Goat instances
+new Goat('img/cruisin-goat.jpg', 'A goat with special accommodations');
+new Goat('img/kissing-goat.jpg', 'Two goats kissing');
+new Goat('img/sassy-goat.jpg', 'Goat with an attitude');
+new Goat('img/smiling-goat.jpg', 'A smiling goat');
+new Goat('img/sweater-goat.jpg', 'Cozy and ready for the winter');
 
-// randomly generate a number
 Goat.randomNum = function() {
+  // randomly generate a number
   var random = Math.random() * Goat.allGoats.length;
-  var roundedDown = math.floor(random);
+  var roundedDown = Math.floor(random);
   return roundedDown;
 };
 
-// make our randomIndex number to feed a position in array Goat.allGoats
 Goat.renderGoat = function() {
   var randomIndex = Goat.randomNum();
+
   var randomGoat = Goat.allGoats[randomIndex];
-  // randomGoat won't be an array, it'll be a new goat for each event.
 
-  // modify the alt attribute
-  Goat.imgElement.alt =randomGoat.altText;
-  // modify the src attribute
-  Goat.imgElement.url =randomGoat.altText;
+  // modify the alt attribute of the img element
+  Goat.imgElement.alt = randomGoat.altText;
 
-
+  // modify the src attribute of the img element
+  Goat.imgElement.src = randomGoat.url;
 };
 
-
-//make new instances, remember that your filepath starts at the HTML document, NOT the js file. This means you don't need ..slash
-new Goat('img filepath, description');
-new Goat('img filepath, description');
-new Goat('img filepath, description');
-
-
-
-// render a random goat on page load:
+// render a random goat on page load
 Goat.renderGoat();
 
-// listen to the image element and add event listener
-Goat.imgElement.addEventListener('click', Goat.randomGoat);
+// listen to the img element
+Goat.imgElement.addEventListener('click', Goat.renderGoat);
+
+// 'use strict';
+// // constructor functions to make goat instances
+// function Goat(filepath, description) {
+//   this.url = filepath;
+//   this.altText = description;
+//   Goat.allGoats.push(this);
+// } //end constructor
+
+// //store those objects in an array
+// Goat.allGoats = [];
+
+
+// // randomly generate a number
+// Goat.randomNum = function() {
+//   var random = Math.random() * Goat.allGoats.length;
+//   var roundedDown = Math.floor(random);
+//   return roundedDown;
+// };
+
+
+// Goat.renderGoat = function() {
+//   // make our randomIndex number to feed a position in array Goat.allGoats
+//   var randomIndex = Goat.randomNum();
+//   var randomGoat = Goat.allGoats[randomIndex];
+//   // randomGoat won't be an array, it'll be a new goat for each event.
+
+//   // modify the alt attribute
+//   Goat.imgElement.altText = randomGoat.altText;
+//   // modify the src attribute
+//   Goat.imgElement.url = randomGoat.altText;
+
+
+// };
+
+
+// //make new instances, remember that your filepath starts at the HTML document, NOT the js file. This means you don't need ..slash
+// new Goat('img src = "img/bag.jpg"', 'your words here');
+// new Goat('img filepath, description');
+// new Goat('img filepath, description');
+
+
+
+// // render a random goat on page load:
+// Goat.renderGoat();
+
+// // listen to the image element and add event listener
+// Goat.imgElement.addEventListener('click', Goat.randomGoat);
